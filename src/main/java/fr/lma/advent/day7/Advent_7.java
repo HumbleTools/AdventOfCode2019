bbc50b4f2b72649c19cf97f95e7d17390dda8b1d
HumbleTools <louis.madeuf@gmail.com>
2019-12-24 00:18:57 +0100

    End of day 7 part 1

--- src/main/java/fr/lma/advent/day7/Advent_7.java
+++ src/main/java/fr/lma/advent/day7/Advent_7.java
@@ -1,61 +1,92 @@
 package fr.lma.advent.day7;
 
-import fr.lma.util.Utils;
-import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.math3.util.CombinatoricsUtils;
-
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.collections4.iterators.PermutationIterator;
+import org.apache.commons.lang3.ArrayUtils;
+
+import fr.lma.util.Utils;
 
 public class Advent_7 {
 
-    public static void main(final String... args) {
-        // Part 1
-        final Iterator<int[]> phaseCombinations = CombinatoricsUtils.combinationsIterator(5, 5);
-        while (phaseCombinations.hasNext()) {
-            final int[] phases = phaseCombinations.next();
-            compute(ArrayUtils.clone(getInput("\\advent7\\day7_input_e1.txt")));
-        }
-    }
+	public static int currentAmpValue = 0;
+	public static int maxAmpValue = 0;
+	public static int currentPhase = 0;
+	private static boolean toggle = false;
 
-    static void compute(final Integer[] intInput) {
-        Operation currentOperation;
-        try {
-            for (int index = 0; index < intInput.length; ) {
-                currentOperation = new Operation(index, intInput);
-                final OperationResult result = currentOperation.compute();
-                if (null == result) {
-                    continue;
-                }
-                if (result.isIndex()) {
-                    index = result.getOutput();
-                } else {
-                    index += currentOperation.getSize();
-                    final Integer output = result.getOutput();
-                    System.out.println(output);
-                }
-            }
-        } catch (final UnsupportedOperationException uoe) {
-            System.out.println(uoe.getMessage());
-        }
-    }
+	public static void main(final String... args) {
+		// Part 1
+		final Iterator<List<Integer>> phasePermutations = new PermutationIterator<>(
+				Arrays.asList(0, 1, 2, 3, 4));
+		while (phasePermutations.hasNext()) {
+			final List<Integer> phases = phasePermutations.next();
+			System.out.println("-------------" + phases);
+			int ampIndex = 1;
+			for (final Integer phase : phases) {
+				currentPhase = phase;
+				final int oldAmpValue = currentAmpValue;
+				currentAmpValue = compute(getInput("\\advent7\\day7_input_louisperso.txt"));
+				System.out.println(
+						String.format(">AMP%1$s (phase %2$s | input %3$s | output %4$s)",
+								ampIndex,
+								currentPhase,
+								oldAmpValue,
+								currentAmpValue));
+				ampIndex++;
+			}
+			if (currentAmpValue > maxAmpValue) {
+				maxAmpValue = currentAmpValue;
+			}
+			currentAmpValue = 0;
+			ampIndex = 1;
+		}
+		System.out.println("The maximum is : " + maxAmpValue);
+	}
 
-    static Integer[] getInput(final String fileName, final Integer noun, final Integer verb) {
-        final String input = Utils.getStringsInput(fileName).findFirst().get();
-        final Integer[] intInput = Arrays.stream(input.split(","))
-                .map(Integer::valueOf)
-                .toArray(Integer[]::new);
-        if (null != noun) {
-            intInput[1] = noun;
-        }
-        if (null != verb) {
-            intInput[2] = verb;
-        }
-        return intInput;
-    }
+	private static Integer compute(final Integer[] intInput) {
+		Operation currentOperation;
+		Integer finalOutput = null;
+		try {
+			for (int index = 0; index < intInput.length;) {
+				currentOperation = new Operation(index, intInput);
+				final OperationResult result = currentOperation.compute();
+				if (result.isIndex()) {
+					index = result.getOutput();
+				} else {
+					finalOutput = result.getOutput();
+					index += currentOperation.getSize();
+				}
+			}
+		} catch (final UnsupportedOperationException uoe) {
+			// System.out.println(uoe.getMessage());
+		}
+		return finalOutput;
+	}
 
-    static Integer[] getInput(final String fileName) {
-        return getInput(fileName, null, null);
-    }
+	private static Integer[] getInput(final String fileName, final Integer noun, final Integer verb) {
+		final String input = Utils.getStringsInput(fileName).findFirst().get();
+		final Integer[] intInput = Arrays.stream(input.split(","))
+				.map(Integer::valueOf)
+				.toArray(Integer[]::new);
+		if (null != noun) {
+			intInput[1] = noun;
+		}
+		if (null != verb) {
+			intInput[2] = verb;
+		}
+		return ArrayUtils.clone(intInput);
+	}
+
+	private static Integer[] getInput(final String fileName) {
+		return getInput(fileName, null, null);
+	}
+
+	public static int getOperationInput() {
+		final int returnValue = toggle ? currentAmpValue : currentPhase;
+		toggle = !toggle;
+		return returnValue;
+	}
 
 }
--- src/main/java/fr/lma/advent/day7/Opcode.java
+++ src/main/java/fr/lma/advent/day7/Opcode.java
@@ -2,154 +2,161 @@ package fr.lma.advent.day7;
 
 public enum Opcode {
 
-    /**
-     * Sums the first two parameters and stores the result at the index shown by the third parameter.
-     */
-    SUM(1, 3, (operation) -> {
-        final Integer paramValue0 = getParamValueFromMode(0, operation);
-        final Integer paramValue1 = getParamValueFromMode(1, operation);
-        final Integer result = paramValue0 + paramValue1;
-        operation.getFullInput()[operation.getParams()[2]] = result;
-        return null;
-    }),
+	/**
+	 * Sums the first two parameters and stores the result at the index shown by the
+	 * third parameter.
+	 */
+	SUM(1, 3, (operation) -> {
+		final Integer paramValue0 = getParamValueFromMode(0, operation);
+		final Integer paramValue1 = getParamValueFromMode(1, operation);
+		final Integer result = paramValue0 + paramValue1;
+		operation.getFullInput()[operation.getParams()[2]] = result;
+		return new OperationResult();
+	}),
 
-    /**
-     * Multiplies the first two parameters and stores the result at the index shown by the third parameter.
-     */
-    MULTIPLY(2, 3, (operation) -> {
-        final Integer paramValue0 = getParamValueFromMode(0, operation);
-        final Integer paramValue1 = getParamValueFromMode(1, operation);
-        final Integer result = paramValue0 * paramValue1;
-        operation.getFullInput()[operation.getParams()[2]] = result;
-        return null;
-    }),
+	/**
+	 * Multiplies the first two parameters and stores the result at the index shown
+	 * by the third parameter.
+	 */
+	MULTIPLY(2, 3, (operation) -> {
+		final Integer paramValue0 = getParamValueFromMode(0, operation);
+		final Integer paramValue1 = getParamValueFromMode(1, operation);
+		final Integer result = paramValue0 * paramValue1;
+		operation.getFullInput()[operation.getParams()[2]] = result;
+		return new OperationResult();
+	}),
 
-    /**
-     * Gets user input and stores it at the index shown by the first parameter. At the the moment, input is faked.
-     */
-    USER_INPUT(3, 1, (operation) -> {
-        final Integer param0 = operation.getParams()[0];
-        operation.getFullInput()[param0] = Advent_7.fakeUserInput;
-        return null;
-    }),
+	/**
+	 * Gets user input and stores it at the index shown by the first parameter. At
+	 * the the moment, input is faked.
+	 */
+	USER_INPUT(3, 1, (operation) -> {
+		final Integer param0 = operation.getParams()[0];
+		operation.getFullInput()[param0] = Advent_7.getOperationInput();
+		return new OperationResult();
+	}),
 
-    /**
-     * Outputs the value of the first parameter.
-     */
-    OUTPUT(4, 1, (operation) -> {
-        return new OperationResult(false, getParamValueFromMode(0, operation));
-    }),
+	/**
+	 * Outputs the value of the first parameter.
+	 */
+	OUTPUT(4, 1, (operation) -> {
+		return new OperationResult(false, getParamValueFromMode(0, operation));
+	}),
 
-    /**
-     * If the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-     */
-    JUMP_IF_TRUE(5, 2, (operation) -> {
-        final Integer paramValue0 = getParamValueFromMode(0, operation);
-        final OperationResult result = new OperationResult(true);
-        if (0 != paramValue0) {
-            result.setOutput(getParamValueFromMode(1, operation));
-        }
-        return result;
-    }),
+	/**
+	 * If the first parameter is non-zero, it sets the instruction pointer to the
+	 * value from the second parameter. Otherwise, it does nothing.
+	 */
+	JUMP_IF_TRUE(5, 2, (operation) -> {
+		final Integer paramValue0 = getParamValueFromMode(0, operation);
+		final OperationResult result = new OperationResult(true);
+		if (0 != paramValue0) {
+			result.setOutput(getParamValueFromMode(1, operation));
+		}
+		return result;
+	}),
 
-    /**
-     * If the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-     */
-    JUMP_IF_FALSE(6, 2, (operation) -> {
-        final Integer paramValue0 = getParamValueFromMode(0, operation);
-        final OperationResult result = new OperationResult(true);
-        if (0 == paramValue0) {
-            result.setOutput(getParamValueFromMode(1, operation));
-        }
-        return result;
-    }),
+	/**
+	 * If the first parameter is zero, it sets the instruction pointer to the value
+	 * from the second parameter. Otherwise, it does nothing.
+	 */
+	JUMP_IF_FALSE(6, 2, (operation) -> {
+		final Integer paramValue0 = getParamValueFromMode(0, operation);
+		final OperationResult result = new OperationResult(true);
+		if (0 == paramValue0) {
+			result.setOutput(getParamValueFromMode(1, operation));
+		}
+		return result;
+	}),
 
-    /**
-     * If the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-     */
-    IS_LESS_THAN(7, 3, (operation) -> {
-        final Integer paramValue0 = getParamValueFromMode(0, operation);
-        final Integer paramValue1 = getParamValueFromMode(1, operation);
-        final Integer paramValue2 = operation.getParams()[2];
-        if (paramValue0 < paramValue1) {
-            operation.getFullInput()[paramValue2] = 1;
-        } else {
-            operation.getFullInput()[paramValue2] = 0;
-        }
-        return new OperationResult();
-    }),
+	/**
+	 * If the first parameter is less than the second parameter, it stores 1 in the
+	 * position given by the third parameter. Otherwise, it stores 0.
+	 */
+	IS_LESS_THAN(7, 3, (operation) -> {
+		final Integer paramValue0 = getParamValueFromMode(0, operation);
+		final Integer paramValue1 = getParamValueFromMode(1, operation);
+		final Integer paramValue2 = operation.getParams()[2];
+		if (paramValue0 < paramValue1) {
+			operation.getFullInput()[paramValue2] = 1;
+		} else {
+			operation.getFullInput()[paramValue2] = 0;
+		}
+		return new OperationResult();
+	}),
 
-    /**
-     * If the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-     */
-    IS_EQUALS(8, 3, (operation) -> {
-        final Integer paramValue0 = getParamValueFromMode(0, operation);
-        final Integer paramValue1 = getParamValueFromMode(1, operation);
-        final Integer paramValue2 = operation.getParams()[2];
-        if (paramValue0.equals(paramValue1)) {
-            operation.getFullInput()[paramValue2] = 1;
-        } else {
-            operation.getFullInput()[paramValue2] = 0;
-        }
-        return new OperationResult();
-    }),
+	/**
+	 * If the first parameter is equal to the second parameter, it stores 1 in the
+	 * position given by the third parameter. Otherwise, it stores 0.
+	 */
+	IS_EQUALS(8, 3, (operation) -> {
+		final Integer paramValue0 = getParamValueFromMode(0, operation);
+		final Integer paramValue1 = getParamValueFromMode(1, operation);
+		final Integer paramValue2 = operation.getParams()[2];
+		if (paramValue0.equals(paramValue1)) {
+			operation.getFullInput()[paramValue2] = 1;
+		} else {
+			operation.getFullInput()[paramValue2] = 0;
+		}
+		return new OperationResult();
+	}),
 
-    /**
-     * If encountered, it stops the main program.
-     */
-    STOP(99, 0, (operation) -> {
-        throw new UnsupportedOperationException("Halt.");
-    });
+	/**
+	 * If encountered, it stops the main program.
+	 */
+	STOP(99, 0, (operation) -> {
+		throw new UnsupportedOperationException("Halt.");
+	});
 
-    private OperationStrategy strategy;
-    private Integer pureOpcode;
-    private Integer paramCount;
+	private OperationStrategy strategy;
+	private Integer pureOpcode;
+	private Integer paramCount;
 
-    Opcode(final int pureOpcode, final int paramCount, final OperationStrategy strategy) {
-        this.pureOpcode = pureOpcode;
-        this.paramCount = paramCount;
-        this.strategy = strategy;
-    }
+	Opcode(final int pureOpcode, final int paramCount, final OperationStrategy strategy) {
+		this.pureOpcode = pureOpcode;
+		this.paramCount = paramCount;
+		this.strategy = strategy;
+	}
 
-    public static Opcode resolve(final Integer value) {
-        Opcode result = null;
-        for (final Opcode opcode : Opcode.values()) {
-            if (value.equals(opcode.pureOpcode)) {
-                result = opcode;
-                break;
-            }
-        }
-        if (null == result) {
-            throw new UnsupportedOperationException("Unknown opcode.");
-        }
-        return result;
-    }
+	public static Opcode resolve(final Integer value) {
+		Opcode result = null;
+		for (final Opcode opcode : Opcode.values()) {
+			if (value.equals(opcode.pureOpcode)) {
+				result = opcode;
+				break;
+			}
+		}
+		if (null == result) {
+			throw new UnsupportedOperationException("Unknown opcode.");
+		}
+		return result;
+	}
 
-    public OperationResult compute(final Operation operation) {
-        return strategy.compute(operation);
-    }
+	public OperationResult compute(final Operation operation) {
+		return strategy.compute(operation);
+	}
 
-    private static Integer getParamValueFromMode(final Integer paramIndex, final Operation operation) {
-        final Operation.Mode paramMode = operation.getParamModes()[paramIndex];
-        final Integer paramValue = operation.getParams()[paramIndex];
-        if (paramMode == Operation.Mode.IMMEDIATE) {
-            return paramValue;
-        } else if (paramMode == Operation.Mode.POSITION) {
-            return operation.getFullInput()[paramValue];
-        } else {
-            throw new UnsupportedOperationException(String.format("Mode %s inconnu !", paramMode));
-        }
-    }
+	private static Integer getParamValueFromMode(final Integer paramIndex, final Operation operation) {
+		final Operation.Mode paramMode = operation.getParamModes()[paramIndex];
+		final Integer paramValue = operation.getParams()[paramIndex];
+		if (paramMode == Operation.Mode.IMMEDIATE) {
+			return paramValue;
+		} else if (paramMode == Operation.Mode.POSITION) {
+			return operation.getFullInput()[paramValue];
+		} else {
+			throw new UnsupportedOperationException(String.format("Mode %s inconnu !", paramMode));
+		}
+	}
 
-    public OperationStrategy getStrategy() {
-        return strategy;
-    }
+	public OperationStrategy getStrategy() {
+		return strategy;
+	}
 
-    public Integer getPureOpcode() {
-        return pureOpcode;
-    }
+	public Integer getPureOpcode() {
+		return pureOpcode;
+	}
 
-    public Integer getParamCount() {
-        return paramCount;
-    }
+	public Integer getParamCount() {
+		return paramCount;
+	}
 }
